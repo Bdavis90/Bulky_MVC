b303bb09f41c40468b54d5480bb4ea81accbe31f
Brandon Davis <wowzerss90@yahoo.com>
2024-09-15 22:16:08 -0400

    Created Category controller and view and seeded database with data. Also added bootstrap theme and icons.

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Models.Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
--- BulkyBook/Data/ApplicationDbContext.cs
+++ BulkyBook/Data/ApplicationDbContext.cs
@@ -10,7 +10,19 @@ namespace BulkyBook.Data
         public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var cats = new Category[] {
+                new Category { Id = 1, Name="Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name="SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name="Comedy", DisplayOrder = 3 },
+                new Category { Id = 4, Name="Thriller", DisplayOrder = 4 },
+                new Category { Id = 5, Name="Horror", DisplayOrder = 5 },
+            };
+            modelBuilder.Entity<Category>().HasData(cats);
         }
     }
 }
